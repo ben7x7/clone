b7b1a6f2ccd23cfe2b0820490301cf2ebba394f9
benoit bessiere <benoit.bessiere@gmail.com>
2020-07-06 17:16:46 +0200

    bundle update

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -9,7 +9,7 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = '';
       } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
       }
     });
     //Burger animation
